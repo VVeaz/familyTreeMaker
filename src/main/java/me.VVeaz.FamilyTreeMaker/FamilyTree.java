6ab7d223cc9da5683e1876a533aa4bf2fe1e5b4b
VVeaz <karolina_wlaz@wp.pl>
2019-02-10 14:41:16 +0100

    better organization of files

--- src/main/java/me.VVeaz.FamilyTreeMaker/FamilyTree.java
+++ src/main/java/me.VVeaz.FamilyTreeMaker/FamilyTree.java
@@ -12,7 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
