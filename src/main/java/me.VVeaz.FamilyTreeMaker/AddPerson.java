8870a9ee7ab0b2ad67d0071016bb6b394de0d824
VVeaz <karolina_wlaz@wp.pl>
2019-02-10 14:39:33 +0100

    better organization of files

---
+++ src/main/java/me.VVeaz.FamilyTreeMaker/AddPerson.java
@@ -0,0 +1,148 @@
+package me.VVeaz.FamilyTreeMaker;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class AddPerson extends JFrame implements ActionListener {
+    private final FamilyTree window;
+    private JButton jButton1Add;
+    private JTextField jTextField1Name;
+    private JTextField jTextField2SecondName;
+    private JTextField jTextField3Surname;
+    private JTextField jTextField4DateOfBirth;
+    private JTextField jTextField5DateOfDeath;
+    private JRadioButton option1Woman;
+    private JRadioButton option2Man;
+    private ButtonGroup sex;
+
+    AddPerson(FamilyTree window){
+
+        super("Add person");
+        this.window = window;
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        jTextField1Name =new JTextField("Name");
+        jTextField1Name.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent fEvt) {
+                JTextField tField = (JTextField)fEvt.getSource();
+                tField.selectAll();
+            }
+        });
+
+        jTextField2SecondName =new JTextField("Second name");
+        jTextField2SecondName.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent fEvt) {
+                JTextField tField = (JTextField)fEvt.getSource();
+                tField.selectAll();
+            }
+        });
+
+        jTextField3Surname =new JTextField("Surname");
+        jTextField3Surname.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent fEvt) {
+                JTextField tField = (JTextField)fEvt.getSource();
+                tField.selectAll();
+            }
+        });
+
+        jTextField4DateOfBirth =new JTextField("Date of birth");
+        jTextField4DateOfBirth.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent fEvt) {
+                JTextField tField = (JTextField)fEvt.getSource();
+                tField.selectAll();
+            }
+        });
+
+        jTextField5DateOfDeath =new JTextField("Date of death");
+        jTextField5DateOfDeath.addFocusListener(new FocusAdapter() {
+            public void focusGained(FocusEvent fEvt) {
+                JTextField tField = (JTextField)fEvt.getSource();
+                tField.selectAll();
+            }
+        });
+
+
+        JPanel panel1 = new JPanel();
+        panel1.setLayout(new BoxLayout(panel1, BoxLayout.PAGE_AXIS));
+
+        panel1.add(jTextField1Name);
+        panel1.add(Box.createRigidArea(new Dimension(0,10)));
+        panel1.add(jTextField2SecondName);
+        panel1.add(Box.createRigidArea(new Dimension(0,10)));
+        panel1.add(jTextField3Surname);
+        panel1.add(Box.createRigidArea(new Dimension(0,10)));
+        panel1.add(jTextField4DateOfBirth);
+        panel1.add(Box.createRigidArea(new Dimension(0,10)));
+        panel1.add(jTextField5DateOfDeath);
+
+        option1Woman = new JRadioButton("Woman",true);
+        option2Man = new JRadioButton("Man");
+        sex = new ButtonGroup();
+        sex.add(option1Woman);
+        sex.add(option2Man);
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new FlowLayout());
+        panel2.add(option1Woman);
+        panel2.add(option2Man);
+
+
+
+        jButton1Add =new JButton("Add person");
+        jButton1Add.addActionListener(this);
+
+        panel1.add(panel2);
+        panel1.add(jButton1Add);
+
+
+        setLayout(new GridBagLayout());
+        add(panel1);
+
+        setSize(375,300);
+
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                int i=JOptionPane.showConfirmDialog(null, "Are you sure you want to close this window?");
+                if(i==0){
+                    window.setAddingNewChild(false);
+                    window.setAddingNewParent(false);
+                    close();
+                }
+                //close();
+            }
+
+        });
+        setVisible(true);
+        setLocationRelativeTo(null);
+
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String nameText= jTextField1Name.getText();
+        String second_nameText= jTextField2SecondName.getText();
+        String surnameText= jTextField3Surname.getText();
+        String date_of_birthText = jTextField4DateOfBirth.getText();
+        String date_of_deathText= jTextField5DateOfDeath.getText();
+
+        Person newPerson =new Person(nameText,second_nameText,surnameText,date_of_birthText,date_of_deathText);
+
+        if(option2Man.isSelected()){ //we are adding man
+            newPerson.setWomen(false);
+            if(window.isAddingNewParent() && window.getChildInAMoment().hasFather()){
+                JOptionPane.showMessageDialog(null, window.getChildInAMoment().toString()+" has father.");
+            }else{
+
+                window.updateGraph(newPerson);
+                this.dispose();
+            }
+        }else if(window.isAddingNewParent()&&window.getChildInAMoment().hasMother()){
+            JOptionPane.showMessageDialog(null, window.getChildInAMoment().toString()+" has mother.");
+        }else {
+            window.updateGraph(newPerson);
+            this.dispose();
+        }
+    }
+
+    private void close(){
+        this.dispose();
+    }
+}
---
+++ src/main/java/me.VVeaz.FamilyTreeMaker/Choose.java
@@ -0,0 +1,108 @@
+package me.VVeaz.FamilyTreeMaker;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Choose extends JFrame {
+    private final FamilyTree window;
+    private PersonTableModel tableModel;
+    private JTable table;
+    private JScrollPane scrollPane;
+    private JPanel panel;
+    private boolean addingChild;
+    private Person first;
+
+    Choose(FamilyTree window, boolean addingChild, Person first){
+        super(addingChild ? "Choose child":"Choose parent");
+        this.window = window;
+        this.addingChild = addingChild;
+        this.first = first;
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setSize(600,700);
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                int i=JOptionPane.showConfirmDialog(null, "Are you sure you want to close this window?");
+                if(i==0){
+
+                    close();
+                }
+            }
+
+        });
+        setLocationRelativeTo(null);
+        showTable();
+    }
+
+    private void showTable() {
+        List<Person> familyWithoutFirst = new ArrayList<>();
+        for(Person p : window.getFamily()){
+            if(p!=first){
+                familyWithoutFirst.add(p);
+            }
+        }
+        tableModel = new PersonTableModel(familyWithoutFirst);
+        table = new JTable(tableModel);
+        scrollPane = new JScrollPane(table);
+
+        scrollPane.setPreferredSize(new Dimension(500, 600));
+        panel = new JPanel();
+        panel.add(scrollPane);
+        add(panel, BorderLayout.CENTER);
+        table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            public void valueChanged(ListSelectionEvent event) {
+
+                Person second = tableModel.getPerson(table.getSelectedRow());
+                if (!addingChild && first.hasFather() && !second.isWomen()) {
+                    JOptionPane.showMessageDialog(null, first.toString() + " has father.");
+                    return;
+                }
+                if (!addingChild && first.hasMother() && second.isWomen()) {
+                    JOptionPane.showMessageDialog(null, first.toString() + " has mother.");
+                    return;
+                }
+                if (addingChild && second.hasBothParents()) {
+                    JOptionPane.showMessageDialog(null, second.toString() + " has both parents.");
+                    return;
+                }
+                if (addingChild && second.hasFather() && !first.isWomen()){
+                    JOptionPane.showMessageDialog(null, second.toString() + " has father");
+                    return;
+                }
+                if (addingChild && second.hasMother() && first.isWomen()){
+                    JOptionPane.showMessageDialog(null, second.toString() + " has mother");
+                    return;
+                }
+                int i = JOptionPane.showConfirmDialog(null, "Are you sure you choose " + second.toString() + "?");
+                if (i == 0) {
+                    if (addingChild) {
+                        //first - parent
+                        first.addChildren(second);
+                        window.drawEdge(first, second);
+                    } else {
+                        //second - parent
+                        second.addChildren(first);
+                        window.drawEdge(second, first);
+                    }
+                    close();
+                }
+            }
+
+
+        });
+
+        setVisible(true);
+        this.validate();
+        this.repaint();
+    }
+
+    private void close(){
+        this.dispose();
+    }
+}
